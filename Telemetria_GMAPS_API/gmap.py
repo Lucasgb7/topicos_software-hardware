c981830347a37085267e338dde77757cd1b177d8
Lucas <lucasgb7_ks@hotmail.com>
2021-08-13 10:10:05 -0300

    telemetria - mostrando gaspar

---
+++ Telemetria_GMAPS_API/gmap.py
@@ -0,0 +1,31 @@
+import gmaps
+from gmaps import locations
+import gmaps.datasets
+from gmaps.heatmap import heatmap_layer
+
+import api
+# autenticando credenciais
+gmaps.configure(api_key = api.api_key)
+
+# dataset de terremotos 
+earthquake_df = gmaps.datasets.load_dataset_as_df('earthquakes')
+print(earthquake_df.head())
+
+# definicao de coordenadas 
+locations = earthquake_df[['latitude', 'longitude']]
+weights = earthquake_df['magnitude']
+fig = gmaps.figure()
+fig.add_layer(gmaps.heatmap_layer(locations, weights = weights))
+fig
+
+fig = gmaps.figure(map_type= 'SATELLITE')
+fig
+
+locations = [(51.5, 0.1), (51.7, 0.2), (51.4, -0.2), (51.49, 0.1)]
+heatmap_layer = gmaps.heatmap_layer(locations)
+fig.add_layer(heatmap_layer)
+
+# definindo coordenadas de Gaspar
+gaspar_coordinates = (-26.93, -48.95)
+fig = gmaps.figure(center = gaspar_coordinates, zoom_level = 12, map_type= 'HYBRID')
+fig
