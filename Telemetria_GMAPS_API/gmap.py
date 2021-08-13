454c3047cea1bc20e3517710318776e4d582ae89
Lucas <lucasgb7_ks@hotmail.com>
2021-08-13 15:43:08 -0300

    comentarios

--- Telemetria_GMAPS_API/gmap.py
+++ Telemetria_GMAPS_API/gmap.py
@@ -9,18 +9,21 @@ gmaps.configure(api_key = api.api_key)
 
 # dataset de terremotos 
 earthquake_df = gmaps.datasets.load_dataset_as_df('earthquakes')
+print('Coordenadas de Terremotos:')
 print(earthquake_df.head())
 
-# definicao de coordenadas 
+# definicao de coordenadas dos terremotos
 locations = earthquake_df[['latitude', 'longitude']]
 weights = earthquake_df['magnitude']
 fig = gmaps.figure()
 fig.add_layer(gmaps.heatmap_layer(locations, weights = weights))
 fig
 
+# exibindo mapa em modo satelite
 fig = gmaps.figure(map_type= 'SATELLITE')
 fig
 
+# exibindo localizacoes especificas
 locations = [(51.5, 0.1), (51.7, 0.2), (51.4, -0.2), (51.49, 0.1)]
 heatmap_layer = gmaps.heatmap_layer(locations)
 fig.add_layer(heatmap_layer)
