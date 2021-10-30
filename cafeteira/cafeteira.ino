e59fedb980cbc9b01be2a6003b3837fe25caa1ce
Lucas Cunha <lucasgb7_ks@hotmail.com>
2021-10-30 10:15:50 -0300

    comentarios

--- cafeteira/cafeteira.ino
+++ cafeteira/cafeteira.ino
@@ -1,41 +1,32 @@
 #include <Servo.h>
 #include <DHT.h>
 
-// Sensor pins
-#define DHTPIN 6
-#define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
+// Sinais de controle do motor e sensor de agua
 #define SERVO_PIN 7
 #define WATER_PIN A0
 
 // Sinais para outros grupos
 #define FILL 5
 #define LID 3
-const unsigned long waterDelay = 70*1000; // 70 segundos
+const unsigned long waterDelay = 7*1000; // 70 segundos
 
 Servo s;
 int pos;
 /* Change these values based on your calibration values */
 int lowerThreshold = 235;
 int upperThreshold = 260;
-int idealThreshold = 250;
+int idealThreshold = 120;
 
 int readWaterLevel() {
   int level = analogRead(WATER_PIN);
   
   // Etapa para verificar o nivel conforme seu threshold
-  if (level == 0) {
-    Serial.print("Water Level: Empty ("); Serial.print(level); Serial.println(")");
+  if (level <= idealThreshold) {
+    Serial.print("Nivel de Agua: Baixo ("); Serial.print(level); Serial.println(")");
+  } else {
+    Serial.print("Nivel de Agua: OK ("); Serial.print(level); Serial.println(")");
   }
-  else if (level > 0 && level <= lowerThreshold) {
-    Serial.print("Water Level: Low ("); Serial.print(level); Serial.println(")");
-  }
-  else if (level > lowerThreshold && level <= upperThreshold) {
-    Serial.print("Water Level: Medium ("); Serial.print(level); Serial.println(")");
-  }
-  else if (level > upperThreshold) {
-    Serial.print("Water Level: High ("); Serial.print(level); Serial.println(")");
-  }
+  
   return level;
 }
 
@@ -43,18 +34,22 @@ int readWaterLevel() {
 void throwWater(bool maxWater, bool lid) {
   // Se a tampa da cafeteira estiver aberta e a agua estiver no nivel maximo, despeja a agua.
   if(lid && maxWater){
+      Serial.println("Liberando fluxo de agua");
       // Libera o fluxo de agua
-      for(pos = 90; pos > 0; pos--){
-        s.write(pos);
-        delay(15);
-      }    
-      delay(waterDelay);
-      
-      // Bloqueia o fluxo de agua
-      for(pos = 0; pos <90; pos++){
+      for(pos = 0; pos < 90; pos++){
         s.write(pos);
         delay(15);
       }
+      Serial.println("Evacoando...");
+      // Tempo para vasao de agua
+      delay(waterDelay);
+      Serial.println("Bloqueando fluxo de agua");
+      // Bloqueia o fluxo de agua
+      for(pos = 90; pos > 0; pos--){
+        s.write(pos);
+        delay(15);
+      }
+      delay(waterDelay);
       
   }
 }
@@ -89,7 +84,6 @@ void setup() {
 }
 
 void loop() {
-
     // Lê informação do nivel de agua
     bool maxWater = checkWater();    
     // Verifica se pode encher a garrafa ou não
